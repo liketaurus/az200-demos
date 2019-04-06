da844b560e41d8d440d0d0dff2687db9a0921551
Alexander Babich <alexander.taurus@gmail.com>
2019-04-06 14:06:59 +0300

    Update Program.cs

--- AZ200T02.2/Program.cs
+++ AZ200T02.2/Program.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
+using System.Linq;
 
 namespace AZ200T02._2
 {
@@ -9,9 +10,6 @@ namespace AZ200T02._2
     {
         static void Main(string[] args)
         {
-
-
-
             Console.WriteLine("Hello World!");
         }
         private static async Task cosmosDBDemo()
@@ -22,17 +20,15 @@ namespace AZ200T02._2
             var key = "";
 
             DocumentClient client = new DocumentClient(new Uri(endpoint), key);
-
-
+            
             Uri collectionUri = UriFactory.CreateDocumentCollectionUri(databaseName, collectionName);
-
-
+            
             var document = new { firstName = "Alex", lastName = "Leh" };
             await client.CreateDocumentAsync(collectionUri, document);
-
-
-
-            var query = client.CreateDocumentQuery<Family>(collectionUri, new SqlQuerySpec() { QueryText = "SELECT * FROM f WHERE (f.surname = @lastName)", Parameters = new SqlParameterCollection() { new SqlParameter("@lastName", "Andt") } }, DefaultOptions); var families = query.ToList();
+                       
+            FeedOptions DefaultOptions = null;
+            var query = client.CreateDocumentQuery<Family>(collectionUri, new SqlQuerySpec() { QueryText = "SELECT * FROM f WHERE (f.surname = @lastName)", Parameters = new SqlParameterCollection() { new SqlParameter("@lastName", "Andt") } }, DefaultOptions);
+            var families = query.ToList();
 
         }
     }
